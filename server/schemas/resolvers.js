721ed138814eb742560f0842d6199cfbfe95a26b
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-05-21 21:43:56 -0400

    required typedefs and resolvers and defined a query for getting a user by their ID

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const {User,Tutor}= require('../models');
+const {signToken,AuthenicationError}= require
+
+const stripe = require('stripe')
+
+const resolvers = {
+    Query: {
+        user: async (_, { id }, { currentUser }) => {
+            if (!currentUser) {
+              throw new AuthenticationError(
+                'You must be logged in to view this user.'
+              );
+            }
+            const user = await User.findById(id);
+            if (!user) {
+              throw new Error('No user found with that ID.');
+            }
+    
+            return user;
+          },
+        },
+        tutor: async (_, { id }, { currentUser }) => {
+            if (!currentUser) {
+                throw new AuthenticationError(
+                    'You must be logged in to view this user.'
+                );
+            }
+
+            const tutor = await Tutor.findById(id);
+            if (!tutor) {
+                throw new Error('No tutor found with that ID.');
+                }
+                return tutor;
+            },
+        };
+
+
+        module.exports = resolvers;
+
+
+
