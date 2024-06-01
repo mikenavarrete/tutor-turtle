78a6ddcdcdc1a18979ce4ffd9493af4c7e078ace
Michael Navarrete <mikenavarrete1998@gmail.com>
2024-06-01 18:30:35 -0400

    added flexbox

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,38 +25,8 @@ function App() {
     <div className="App">
       <Navbar />
       <Outlet />
-      {/* <header className="header"> */}
-        {/* <h1>Tutor Turtle</h1>
-        <nav>
-          <a href="#">Become a Tutor</a>
-          <a href="#">Log In</a>
-        </nav>
-      </header>
-      <main className="main">
-        <h2>"Unlock Your Learning Shell with Tutor Turtle!"</h2>
-        <div className="search-bar">
-          <input type="text" value={searchTerm} onChange={handleSearch} placeholder="What would you like to learn?" />
-          <button type="button">Search</button>
-        </div>
-        <div className="subjects-container">
-          {filteredSubjects.map((subject) => (
-            <div key={subject.name} className="subject">
-              <div className="icon">{subject.icon}</div>
-              <span>{subject.name}</span>
-            </div>
-          ))}
-        </div>
-        <div className="tutors-section">
-          <h3>50 million evaluated private tutors ★★★★★</h3>
-          <div className="tutors-grid">
-            <div className="tutor-card">
-              <button className="heart-button">:heart:</button>
-            </div>
-          </div>
-        </div>
-      </main>
-      <footer className="footer"></footer> */
-      } 
+
+       
       <Footer />
     </div>
   );
