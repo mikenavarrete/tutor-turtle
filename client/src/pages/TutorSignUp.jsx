b699d1b77a40c3633e9c00c06f01f0038aca094a
Michael Navarrete <mikenavarrete1998@gmail.com>
2024-06-02 15:54:35 -0400

    added signup forms, not finished*

--- client/src/pages/TutorSignUp.jsx
+++ client/src/pages/TutorSignUp.jsx
@@ -1,3 +1,57 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import '../styles/TutorSignUp.css';
+
 export default function TutorSignUp() {
-    return (<h1>TutorSignUp!</h1>)
+  return (
+
+    <div>
+        <div className="Header">
+            <h1>Welcome to Tutor Turtle! Please enter your user info</h1>
+                <br></br>
+        </div>
+
+            <div className="container">
+             <div class="field">
+            <label class="label">Name</label>
+              <div class="control">
+             <input class="input  is-info is-rounded mb-5" type="text" placeholder="e.g Alex Smith"/>
+             </div>
+            </div>
+
+            <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+          <input class="input  is-info is-rounded mb-5" type="email" placeholder="e.g. alexsmith@gmail.com"/>
+            </div>
+            </div>
+
+            <label className="label is-justify-content-left">Password</label>
+            <input
+            className="input is-info is-rounded mb-5"
+            type="text"
+            placeholder="Info input"
+            />
+
+            <label className="label is-justify-content-left">Confirm Password</label>
+            <input 
+            className="input is-info is-rounded mb-5"
+            type="text"
+            placeholder="Rounded input"
+            />
+
+            <div clasName="select">
+            <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+            </select>
+            </div>
+            <br></br>
+
+            <label className="label is-justify-content-left">Please Enter a Bio</label>
+            <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
+        </div>
+    </div>
+
+  );
 }
