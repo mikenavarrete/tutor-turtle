abdeb18139949b1518a3370b9850264120a6f562
Michael Navarrete <mikenavarrete1998@gmail.com>
2024-06-03 00:10:26 -0400

    made room for images on side

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,29 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import '../styles/Login.css';
+
 export default function Login() {
-    return (<h1>Login!</h1>)
+    return (<div>
+          <label className="label is-justify-content-left">Confirm Password</label>
+            <div className="field is-flex flex-direction-row is-justify-content-center">
+            <input 
+            className="input is-info is-rounded mb-5 custom-width "
+            type="text"
+            placeholder="Rounded input"
+            />
+            <button class="button">Button</button>
+            </div>
+
+            <label className="label is-justify-content-left">Confirm Password</label>
+            <div className="field is-flex flex-direction-row is-justify-content-center">
+            <input 
+            className="input is-info is-rounded mb-5 custom-width "
+            type="text"
+            placeholder="Rounded input"
+            />
+            <button class="button">Button</button>
+            </div>
+
+
+    </div>)
 }
