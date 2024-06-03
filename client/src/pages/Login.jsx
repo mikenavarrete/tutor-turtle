b2d7fdf46343e69bd5444341bcb3b1c7938dadb6
Michael Navarrete <mikenavarrete1998@gmail.com>
2024-06-03 11:00:04 -0400

    centered login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,25 +4,28 @@ import '../styles/Login.css';
 
 export default function Login() {
     return (<div>
-          <label className="label is-justify-content-left">Confirm Password</label>
-            <div className="field is-flex flex-direction-row is-justify-content-center">
-            <input 
-            className="input is-info is-rounded mb-5 custom-width "
-            type="text"
-            placeholder="Rounded input"
-            />
-            <button class="button">Button</button>
-            </div>
+      <div className="login-field is-flex is-flex-direction-column is-align-items-center is-justify-content-center full-height pb-6">
+        <label className="label is-justify-content-left">Confirm Password</label>
+              <div className="field is-flex flex-direction-row is-justify-content-center">
+              <input 
+              className="input is-info is-rounded mb-5 custom-width "
+              type="text"
+              placeholder="Rounded input"
+              />
+              <button class="button">Button</button>
+              </div>
 
-            <label className="label is-justify-content-left">Confirm Password</label>
-            <div className="field is-flex flex-direction-row is-justify-content-center">
-            <input 
-            className="input is-info is-rounded mb-5 custom-width "
-            type="text"
-            placeholder="Rounded input"
-            />
-            <button class="button">Button</button>
-            </div>
+              <label className="label is-justify-content-left">Confirm Password</label>
+              <div className="field is-flex flex-direction-row is-justify-content-center">
+              <input 
+              className="input is-info is-rounded mb-5 custom-width "
+              type="text"
+              placeholder="Rounded input"
+              />
+              <button class="button">Button</button>
+              </div>
+      </div>
+
 
 
     </div>)
